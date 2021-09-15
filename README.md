29bbdf1e9cac68da5292303856792d02b810db75
skdog123 <86732132+skdog123@users.noreply.github.com>
2021-09-15 11:08:23 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Blower1
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+  createSprite(375, 300, 130, 15);
+  button=createButton("Click to Blow")
+  button.position(width/2-10,height-50,)
+  button.class("blowButton")
+  button.mousePressed(blow)
+
+blower = new Blower(width/2+35,265,100,100)
+ball = new Ball(width/2+35,165,50,50)
+var render = Matter.Render.create({ element:document.body, engine:engine, options: { width:800, height:400, wireframes:false } }); Matter.Render.run(render);
+}
+
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+ blower.show()
+ ball.show()
+}
+ 
+
+
+function blow(){
+  Matter.Body.applyForce(ball.body, {x:0,y:0},{x:0,y:0.05})
+}
